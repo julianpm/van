7669c6d93238e853c87c7e333e7024290c9f9751
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-10-23 09:50:08 -0400

    removed unnecessary code in header and footer

--- footer.php
+++ footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'van' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'van' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'van' ), 'van', '<a href="http://julianmeanchoff.com" rel="designer">Julian</a>' ); ?>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
