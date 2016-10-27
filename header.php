a92c0bc3ab6b3a43b3b6ec0d5c8e2b350a96e38f
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-10-27 00:02:07 -0400

    adjusted header markup and styled accordingly

--- header.php
+++ header.php
@@ -26,9 +26,9 @@
 	<header id="masthead" class="site-header" role="banner">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<div class="row">
-				<div class="columns small-12 large-6 large-centered">
+				<div class="columns small-12">
 					<a class="logo" href="<?php esc_url( home_url( '/' ) ); ?>">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/van_logo.png" alt="Van Logo">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/van-logo.png" alt="Van Logo">
 					</a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</div>
