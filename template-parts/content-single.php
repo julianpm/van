2341f9ca09a264330aaf1e05338e6a1a2765ccf2
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-10-28 08:29:27 -0400

    created file structure for single blog posts

---
+++ template-parts/content-single.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package van
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php van_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		
+	</div><!-- .entry-content -->
+
+	<!-- <footer class="entry-footer"> -->
+		<?php #van_entry_footer(); ?>
+	<!-- </footer>.entry-footer -->
+</article><!-- #post-## -->
