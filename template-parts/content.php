859e37ac93fa42127b17583807bf60df39900767
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-10-27 22:57:31 -0400

    began building blog archive

--- template-parts/content.php
+++ template-parts/content.php
@@ -27,21 +27,10 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'van' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'van' ),
-				'after'  => '</div>',
-			) );
-		?>
+		
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php van_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<!-- <footer class="entry-footer"> -->
+		<?php #van_entry_footer(); ?>
+	<!-- </footer>.entry-footer -->
 </article><!-- #post-## -->
