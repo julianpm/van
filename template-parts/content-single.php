660973a647c229473520afbd80d76686dfeebd57
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-10-31 21:51:31 -0400

    added proper content code for single blog post content

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -27,7 +27,19 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		
+		<?php
+			the_content( sprintf(
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'pso' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pso' ),
+				'after'  => '</div>',
+			) );
+
+		?>
 	</div><!-- .entry-content -->
 
 	<!-- <footer class="entry-footer"> -->
