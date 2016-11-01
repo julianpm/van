b71f42d5266aa073061c87b7027eda710dcd7899
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-10-31 21:51:12 -0400

    built markup for single blog post

--- single.php
+++ single.php
@@ -12,24 +12,26 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+			<div class="row section-padding">
+				<div class="columns small-12 large-8">
+					<?php
+					while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', 'single' );
+						get_template_part( 'template-parts/content', 'single' );
 
-			the_post_navigation();
+						#the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+					endwhile; // End of the loop.
+					?>
+				</div>
+				<div class="columns small-12 large-4">
+					<?php get_sidebar(); ?>
+					
+				</div>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
