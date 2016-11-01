9ab990944dee8190d8d35cda704ed76d6aeff411
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-10-31 22:10:03 -0400

    removed whitespace

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -14,7 +14,7 @@ if ( ! function_exists( 'van_posted_on' ) ) :
 function van_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 	}
 
 	$time_string = sprintf( $time_string,
@@ -176,18 +176,3 @@ function van_social_media() {
 		</nav>
 	<?php }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
