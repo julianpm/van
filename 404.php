1a422dcd9fbbf95c251ea23677cb0dfe41e54b98
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-05 13:09:36 -0400

    built and styled 404 page

--- 404.php
+++ 404.php
@@ -17,10 +17,12 @@ get_header(); ?>
 					<div class="error-404-wrapper">
 						<section class="error-404 not-found">
 							<p class="error-404-header"><?php esc_html_e( 'Sorry, page not found !', 'van' ); ?></p>
+							<div class="border"></div>
 							<div class="page-content">
 								<p><?php esc_html_e( 'Apologies, but the page you requested could not be found.', 'van' ); ?></p>
 								<p><?php esc_html_e( 'Perhaps searching will help.', 'van'); ?></p>
 								<?php get_search_form(); ?>
+								<p>Back to <a href="<?php echo esc_url( home_url() ); ?>">home</a> or <a href="<?php echo esc_url( home_url( '/contact' ) ); ?>">contact us</a></p>
 							</div><!-- .page-content -->
 						</section><!-- .error-404 -->
 					</div>
