e72694690ceae695d93472c9a1f18535c600b1c0
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-13 14:58:17 -0500

    put function to remove "archives" label from custom post type

--- functions.php
+++ functions.php
@@ -149,3 +149,23 @@ require get_template_directory() . '/inc/jetpack.php';
 if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page();
 }
+
+// REMOVES "ARCHIVE" LABEL FROM ANY CUSTOM POST ARCHIVE
+// TO GO IN FUNCTIONS.PHP
+function my_theme_archive_title( $title ) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
+    } elseif ( is_tax() ) {
+        $title = single_term_title( '', false );
+    }
+  
+    return $title;
+}
+ 
+add_filter( 'get_the_archive_title', 'my_theme_archive_title' );
