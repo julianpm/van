c8ca05cff6de52b737f84532aa3933f8045fa314
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-13 20:59:12 -0500

    added archive header function

--- archive.php
+++ archive.php
@@ -13,18 +13,9 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) : ?>
+		if ( have_posts() ) :
 
-			<header class="page-header-simple">
-				<div class="row">
-					<div class="columns small-12">
-						<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="archive-description">', '</div>' );
-						?>
-					</div>
-				</div>
-			</header><!-- .page-header -->
+			van_projects_archive_header(); ?>
 
 			<div class="row section-padding">
 				
