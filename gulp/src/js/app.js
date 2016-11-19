094a6be9c010cda80beb2969871b61eccb5efc66
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-19 16:08:17 -0500

    wrote js functionality for flyout nav slideToggle

--- gulp/src/js/app.js
+++ gulp/src/js/app.js
@@ -1,10 +1,16 @@
 jQuery(document).ready(function($){
 
-	//SLIDER
+	// SLIDER
 	$('.slider').slick({
 		dots:true,
 		arrows:false,
 		adaptiveHeight:true
+	});
+
+	// FLYOUT NAV CLICK FUNCTION
+	$('.menu-toggle').click(function(){
+		$('.flyout').slideToggle();
+		$(this).toggleClass('active');
 	});
 
 });
