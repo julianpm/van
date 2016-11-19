3b0957ce26bd7a63e550b83ee5971c810e4e82ac
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-19 15:04:44 -0500

    registered flyout nav in functions.php

--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ function van_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'van' ),
+		'flyout' => esc_html__( 'Flyout', 'cod' )
 	) );
 
 	/*
