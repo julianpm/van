214b5ceef4886b43a81a2887cb400acb36342d95
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-20 01:11:47 -0500

    fixed markup and css for isotope functionality

--- archive.php
+++ archive.php
@@ -18,25 +18,44 @@ get_header(); ?>
 			van_projects_archive_header(); ?>
 
 			<div class="row section-padding">
-				
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post(); ?>
-				
-					<div class="columns small-12 large-4">
-						<?php get_template_part( 'template-parts/content', 'projects' ); ?>
-					</div>
-
-				<?php
-				endwhile;
-
-				else :
-
-					get_template_part( 'template-parts/content', 'none' );
-
-				endif; ?>
-				
+				<div class="columns small-12">
+					<ul class="project-options">
+						<li>
+							<a href="#" data-filter="*">All</a>
+						</li>
+						<li>
+							<a href="#" data-filter=".service-audio">Audio</a>
+						</li>
+						<li>
+							<a href="#" data-filter=".service-video">Video</a>
+						</li>
+						<li>
+							<a href="#" data-filter=".service-design">Design</a>
+						</li>
+						<li>
+							<a href="#" data-filter=".service-branding">Branding</a>
+						</li>
+					</ul>
+				</div>
 			</div>
+				
+			<div class="row">
+				<div class="columns small-12 projects-wrapper">
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post(); ?>
+					
+						<!-- <div class="columns small-12 large-4"> -->
+							<?php get_template_part( 'template-parts/content', 'projects' ); ?>
+						<!-- </div> -->
+
+					<?php
+					endwhile; ?>
+				</div>
+			</div>
+		
+		<?php
+		endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
