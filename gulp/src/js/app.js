eaf659440b028509bdb7a3339d8a1f404d9ce3fe
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-20 01:09:48 -0500

    wrote isotope js for projects archive

--- gulp/src/js/app.js
+++ gulp/src/js/app.js
@@ -16,4 +16,15 @@ jQuery(document).ready(function($){
 		$('.header-right i').toggleClass('fa-bars fa-times');
 	});
 
+	// PROJECTS ARCHIVE ISOTOPE
+	var container = $('.projects-wrapper');
+
+	$('.project-options a').click(function(event){
+		event.preventDefault();
+		var selector = $(this).attr('data-filter');
+		container.isotope({
+			filter: selector
+		});
+	});
+
 });
