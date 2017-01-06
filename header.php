fbbf0dc40adf054c20627e2919de24c00804ca38
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-01-05 22:53:05 -0500

    Changed markup of header so logo is dynamic

--- header.php
+++ header.php
@@ -27,9 +27,14 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<div class="row">
 				<div class="columns small-12">
-					<a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/van-logo.png" alt="Van Logo">
-					</a>
+					<?php
+					$van_logo = get_field( 'van_logo', 'option' );
+
+					if ( $van_logo ){ ?>
+						<a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+							<img src="<?php echo esc_url( $van_logo['url'] ); ?>" alt="<?php echo $van_logo['alt']; ?>">
+						</a>
+					<?php } ?>
 					<div class="header-right">
 						<button>
 							<i class="fa fa-bars" aria-hidden="true"></i>
